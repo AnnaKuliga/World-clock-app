7e4cf40408a0a3f6bb678435a29f88453b987591
AnnaKuliga <118673659+AnnaKuliga@users.noreply.github.com>
2022-12-05 11:35:24 +0100

    Added JS to Warsaw and Honolulu timezone

--- src/app.js
+++ src/app.js
@@ -1,1 +1,26 @@
+function updateTime() {
+  // Warsaw
+  let warsawElement = document.querySelector("#warsaw");
+  let warsawDateElement = warsawElement.querySelector(".date");
+  let warsawTimeElement = warsawElement.querySelector(".time");
+  let warsawTime = moment().tz("Europe/Warsaw");
 
+  warsawDateElement.innerHTML = warsawTime.format("MMMM Do, YYYY");
+  warsawTimeElement.innerHTML = warsawTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  // Honolulu
+  let honoluluElement = document.querySelector("#honolulu");
+  let honoluluDateElement = honoluluElement.querySelector(".date");
+  let honoluluTimeElement = honoluluElement.querySelector(".time");
+  let honoluluTime = moment().tz("Pacific/Honolulu");
+
+  honoluluDateElement.innerHTML = honoluluTime.format("MMMM Do, YYYY");
+  honoluluTimeElement.innerHTML = honoluluTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
